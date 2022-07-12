0b69df8067c243218ca9092bd6b4183542d3865f
kenenisa <53818162+kenenisa@users.noreply.github.com>
2022-07-12 13:22:08 +0300

    changed server config

--- src/Util/request.js
+++ src/Util/request.js
@@ -1,12 +1,15 @@
-const server = window.location.href.includes('192') ? window.location.origin.split(':')[0] + ':5000' :(window.location.href.includes('localhost') ? 'http://localhost:5000/' : window.location.origin + '/')
-// const server = 
+const server = {
+    url: 'https://polar-oasis-87155.herokuapp.com/'
+}
+// const server = window.location.href.includes('192') ? window.location.origin.split(':')[0] + ':5000' :(window.location.href.includes('localhost') ? 'http://localhost:5000/' : window.location.origin + '/')
+
 const func = (to, post = false, body = null) => {
     if (post) {
-        return fetch(server + to, {
+        return fetch(server.url + to, {
             method: "POST", body: JSON.stringify(body),
             // headers: { 'Content-Type': 'application/json' }
-        }).then(e => e.json()).catch(err=>console.log(err));
+        }).then(e => e.json()).catch(err => console.log(err));
     }
-    return fetch(server + to).then(e => e.json()).catch(err=>console.log(err));
+    return fetch(server.url + to).then(e => e.json()).catch(err => console.log(err));
 }
 export default func
